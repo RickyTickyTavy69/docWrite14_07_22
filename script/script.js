f6543d072be2a39a5cd353c840b93dd69a1a931c
artem <bakirovartem69@gmail.com>
2022-07-14 15:40:14 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,9 @@
+document.write("<h1>Hello World!</h1>");
+document.write("<p>Have a nice day!</p>");
+let myText = "<p> Hello again </p>";
+const vorname = "<p>Artem</p>";
+const nachname = "<p>Bakirov</p>";
+
+document.write(myText);
+document.write("Name:", vorname);
+document.write("Nachname", nachname);
